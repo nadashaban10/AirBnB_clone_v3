2a24332712d8ab537d904a461f245b427c1f3e95
nadashaban10 <nadashaban10@github.com>
2024-03-29 17:02:56 +0200

    2.3.4.5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,25 +1,31 @@
 #!/usr/bin/python3
-'''index.py'''
+""" Index """
 from api.v1.views import app_views
 from flask import jsonify
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models.amenity import Amenity
 import models
-from models import amenity, city, place, review, state, user
 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
-    """return status"""
+    """HBNB Status"""
     return jsonify({"status": "OK"})
 
-@app_views.route('/api/v1/stats', strict_slashes=False)
-def some_stats():
-    """return stats"""
-    dict = {
-        "amenities": models.storage.count("Amenity"),
-        "cities": models.storage.count("City"),
-        "places": models.storage.count("Place"),
-        "reviews": models.storage.count("Review"),
-        "states": models.storage.count("State"),
-        "users": models.storage.count("User")
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """HBNB Stats"""
+    my_dict = {
+        "amenities": models.storage.count(Amenity),
+        "cities": models.storage.count(City),
+        "places": models.storage.count(Place),
+        "reviews": models.storage.count(Review),
+        "states": models.storage.count(State),
+        "users": models.storage.count(User)
     }
-    return dict
+    return my_dict
